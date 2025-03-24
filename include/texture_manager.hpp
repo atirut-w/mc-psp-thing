e9c1f9aa57e4eaaa2d6a6ae90a79ae52fe48bd72
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-03-24 16:18:52 +0700

    Reimplement Texture Manager

---
+++ include/texture_manager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "raylib.h"
+#include "resource_location.hpp"
+#include <string>
+#include <unordered_map>
+namespace MCPSP {
+
+class TextureManager {
+  static std::unordered_map<std::string, Texture2D> textureCache;
+
+public:
+  static const Texture2D &getTexture(const ResourceLocation &location) {
+    std::string path = location.resolvePath("textures") + ".png";
+    if (textureCache.find(path) == textureCache.end()) {
+      Texture2D texture = LoadTexture(path.c_str());
+      textureCache[path] = texture;
+    }
+    return textureCache[path];
+  }
+};
+
+}
---
+++ src/texture_manager.cpp
@@ -0,0 +1,7 @@
+#include "texture_manager.hpp"
+
+namespace MCPSP {
+
+std::unordered_map<std::string, Texture2D> TextureManager::textureCache;
+
+}
